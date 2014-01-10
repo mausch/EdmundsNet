0981dba619b8d91f8f5395ce538331d42eb908eb
mausch <mauricioscheffer@gmail.com>
2014-01-10 14:17:20 -0300

    Helper to unpack Choice async in C#

--- CSharpTests/Program.cs
+++ CSharpTests/Program.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EdmundsNet;
 
 namespace CSharpTests {
     class Program {
         static void Main(string[] args) {
-            var edmunds = new EdmundsNet.Service(apiKey: "");
-            var t = FSharpAsync.StartAsTask(edmunds.AsyncLookupByVIN("2G1FC3D33C9165616"), null, null);
-            Console.WriteLine(t.Result);
+            var edmunds = new Service(apiKey: "");
+            var t = edmunds.AsyncLookupByVIN("2G1FC3D33C9165616").GetOrThrow();
+            Console.WriteLine(t);
         }
     }
 }
--- EdmundsNet/Service.fs
+++ EdmundsNet/Service.fs
@@ -3,6 +3,15 @@
 open EdmundsNet.Requests
 open EdmundsNet.Vehicles
 open Fleece
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type ServiceUtils =
+    [<Extension>]
+    static member GetOrThrow(x: _ ChoiceS Async) =
+        match Async.RunSynchronously x with
+        | Choice1Of2 a -> a
+        | Choice2Of2 e -> failwith e
 
 type Service(apiKey: string) =
     member x.AsyncLookupByVIN(vin: string) : VINLookupResponse ChoiceS Async =
